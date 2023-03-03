27fe54c1e0db69f92f5e007619a7d4708d70dae3
prabinadkri <107361341+prabinadkri@users.noreply.github.com>
2023-03-03 17:22:29 +0545

    firsr commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
+import {useState} from "react";
+import FormInput from "./components/FormInput";
+const App=()=> {
+    const [values,setValues] =useState({
+           username:"",
+           email:"",
+           phnumber:"",
+           password:"",
+    })
+ const onChange =(e)=>
+ {
+     setValues({...values,[e.target.name]:e.target.value});
+ }
+ const inputs=[
+     {
+         id:1,
+         name:"username",
+         type:"text",
+         placeholder:"Username",
+         label:"Username"
+     },
+     {
+         id:2,
+         name:"email",
+         type:"text",
+         placeholder:"Email",
+         label:"Email"
+     },
+     {
+         id:3,
+         name:"phnumber",
+         type:"number",
+         placeholder:"Phone number",
+         label:"Phone number"
+     },
+     {
+         id:4,
+         name:"password",
+         type:"password",
+         placeholder:"Password",
+         label:"Password"
+     }
+ ]
+const handleSubmit=(e)=>{
+    e.preventDefault();
+    //Code the task to do when submit is pressed
+   //onst data =new FormData(e.target);
+  //console.log(Object.fromEntries(data.entries()));
+};
+console.log(values);
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <form onSubmit={handleSubmit}>
+     {
+         inputs.map(input=>(
+             <FormInput key={input.id} {...input} value={values[input.name]} onChange={onChange}/>
+         ))
+     }
+     <button>Submit</button>
+      </form>
     </div>
   );
 }
---
+++ src/components/FormInput.js
@@ -0,0 +1,15 @@
+import "./forminput.css"
+const FormInput=(props)=>
+{
+	const{label, onChange,id, ...inputProps} =props;
+	return (
+		<div className="formInput">
+	<label>{label}</label>
+	<input {...inputProps} onChange={onChange} />
+	
+		</div>
+
+	)
+}  
+
+export default FormInput
